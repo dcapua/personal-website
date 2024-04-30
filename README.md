09df1571b63f4a1d89d3d0d46785e9b57518a1a0
Danny Capua <danielcapua19@gmail.com>
2024-04-29 23:20:57 -0500

    React/NextJS rebuild and placeholder redesign

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# personal-website
+This is the repo for my personal website. More to come!
+Built using NextJS
---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="header">
+      <div className="main-header">
+        <img src="/Updated Headshot.png" alt="Danny Capua" id="headshot" />
+        <p id="message">Hello! Content TBD</p>
+      </div>
+    </div>
+  );
+}
