54298214d46b1d241f84a8e9a7571f31034fe553
Danny Capua <danielcapua19@gmail.com>
2024-05-13 22:47:20 -0500

    Refactoring, adjusted for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="header">
       <div className="main-header">
-        <img src="/Updated Headshot.png" alt="Danny Capua" id="headshot" />
-        <p id="message">Hello! Content TBD</p>
+        <img src="/Updated Headshot.png" alt="Danny Capua" className="headshot" />
+        <h1 className="message">Welcome to my page!</h1>
       </div>
     </div>
   )
